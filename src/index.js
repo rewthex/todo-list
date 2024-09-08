173e47aa290ddb400bc5707a0eaa0409149bbcee
Aaron <trapp.root@gmail.com>
2024-09-08 12:13:30 -0700

    Initial structure complete

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.style.css';
+import sidebar from './sidebar';
+import header from './header';
+import tasks from './tasks'
+
+const initialRender = () => {
+    const body = document.querySelector("body")
+
+    
+}
